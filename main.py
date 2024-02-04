cf677b8aab788638cbfd7370005791ec515dcf19
SaibabuAngani <146207336+SaibabuAngani@users.noreply.github.com>
2024-02-04 13:42:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-#create a password generator!
+import random as r
+import string as str
 
-#import random
-import random
-
-#gather our characters
-lower='abcdefghijklmnopqrstuvwxyz'
-upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-symbols='!@#$%^&*()-_+=[]{}:;.,<>'
-numbers='1234567890'
-all=lower+upper+symbols+numbers
-
-#set password length
-length=10
-
-#loop through each character
-password=''
-for _ in range(length):
-    password=''.join([password,random.choice(all)])
+def Password(lenght, use_text=True, use_number=True, use_special_character=True):
+    character =""
+    if use_text:
+        character +=str.ascii_letters
+    if use_number:
+        character +=str.digits
+    if use_special_character:
+        character +=str.punctuation
     
-print(password)
+    if not character:
+        print("error:please select atleast one character type.")
+        return
+    
+    pwd =''.join(r.choice(character) for _ in range(lenght))
+    return pwd
+    
+def main():
+    print("Saibabu Password Generator")
+    
+    try:
+        lenght = int(input("enter your password lenght: "))
+        use_text = input("include letter in your password? (Y/N): ").lower() == 'y'
+        use_number =input("include number in your password? (Y/N): ").lower() =='y'
+        use_special_character =input("include Special character in your password? (Y/N): ").lower() =='y'
+
+        pwd = Password(lenght, use_text, use_number, use_special_character)
+
+        if pwd:
+            print(" Your Generated Password:", pwd)
+        else:
+            print("Password generation failed.")
+    except ValueError:
+        print("Error: Invalid input. Please enter a valid number for password length.")
+if __name__ == "__main__":
+    main()       
